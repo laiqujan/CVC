8f79a3a424db59ccca1e9ed98dbd2c608bb0a9df
laiqujan <ujankhan66@gmail.com>
2016-12-29 23:23:54 +0500

    Contains Add Method

--- HellowGit/src/pack/Demo.java
+++ HellowGit/src/pack/Demo.java
@@ -4,7 +4,12 @@ public class Demo {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		System.out.println("Sum:"+add(4,5));
 
 	}
+
+	public static int add(int num1, int num2) {
+         return num1+num2;
+  	}
 
 }
